3e061371fe5124a5331bee5a5bfe32b2c2dd7bde
Evgeniy Fateev <spaceflow@gmail.com>
2014-01-22 16:39:02 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meetup::Application.config.secret_key_base = 'c3ef6acd12bce7a8d6935d6174de04d39f75f5476bd5ae5221256f49d688af741015de08c653e7eb0cb3d8b0aa94d3d71cbcab767ded0c9db69e3060cd40b952'
